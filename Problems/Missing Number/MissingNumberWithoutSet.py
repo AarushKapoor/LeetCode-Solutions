b9b606389d66096fe3164e0f679f60ca71fa70a1
AarushKapoor <aarushkapoor187@gmail.com>
2026-06-03 15:48:58 -0400

    Create MissingNumberWithoutSet.py

---
+++ Problems/Missing Number/MissingNumberWithoutSet.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        ans = 0
+
+        for i in range(len(nums) + 1):
+            ans += i
+        
+        for num in nums:
+            ans -= num
+
+        return ans
